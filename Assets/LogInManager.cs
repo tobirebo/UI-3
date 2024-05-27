a32e00fdaa8fbc253515c11af9520829b88c2a32
REBOREDO TOBÍAS <48115989@ort>
2024-05-27 09:03:11 -0300

    contraseña seteada

--- Assets/LogInManager.cs
+++ Assets/LogInManager.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LogInManager : MonoBehaviour
 {
+    public string contrasenia = "matoso246";
+    public InputField contraseniauser;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,16 @@ public class LogInManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void BotonPresionado()
+    {
+        if (contrasenia == contraseniauser.text)
+        {
+            Debug.Log("entraste");
+        }
+        else
+        {
+            Debug.Log("no entraste");
+        }
     }
 }
